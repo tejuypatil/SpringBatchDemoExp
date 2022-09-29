320adfc7cb489e227ceaecdec26e69b5bba26f93
tejuypatil <tejuypatil@gmail.com>
2022-09-29 15:31:30 +0530

    Started demo example of spring batch

--- src/main/java/com/example/springbatchexample/controller/LoadController.java
+++ src/main/java/com/example/springbatchexample/controller/LoadController.java
@@ -1,29 +1,42 @@
 package com.example.springbatchexample.controller;
 
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/load")
 public class LoadController {
-//    @Autowired
-//    JobLauncher jobLauncher;
-//
-//    @Autowired
-//    Job job;
-//
-//    @GetMapping
-//    public void load(){
-//
-//        JobParameters parameters = new JobParameters(maps);
-//        jobLauncher.run(job, parameters);
-//    }
+    @Autowired
+    JobLauncher jobLauncher;
+
+    @Autowired
+    Job job;
+
+    @GetMapping
+    public BatchStatus load() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+
+        Map<String, JobParameter> maps = new HashMap<>();
+        maps.put("time", new JobParameter(System.currentTimeMillis()));
+        JobParameters parameters = new JobParameters(maps);
+        JobExecution jobExecution = jobLauncher.run(job, parameters);
+        System.out.println("JobExecution: "+ jobExecution.getStatus());
+        System.out.println("Batch is Running...");
+        while (jobExecution.isRunning()){
+            System.out.println("...");
+        }
+        return jobExecution.getStatus();
+    }
 
 }
